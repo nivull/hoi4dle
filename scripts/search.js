aea92fc68b9ea927e0d2bc6bc10e8b2f27db36e9
nivull <biscaynemac@gmail.com>
2026-07-07 00:44:59 -0400

    Added all of europe manually to the json lol

--- scripts/search.js
+++ scripts/search.js
@@ -1,4 +1,5 @@
 let data;
+const list = document.getElementById('countries');
 async function loadData() {
   const response = await fetch('./scripts/countries.json');
   data = await response.json();
@@ -8,25 +9,44 @@ loadData();
 const inputbar = document.getElementById("input-bar");
 
 inputbar.addEventListener('input', (event)=> {
+    list.replaceChildren();
     if(event.target.value != ''){
         findMatching(event.target.value);
     } else {
-        
+
     }
 });
 
 function findMatching(input) {
 
+    let first = [];
     const matching = data.filter((elem)=> {
 
         if(elem.country.toLowerCase().includes(input.toLowerCase())) {
+            //if first letter
+            if(elem.country.toLowerCase()[0] == input.toLowerCase()){
+                first.push(elem);
+                return false;
+            }
             return true;
         } else{
             return false;
         }
 
     });
-    console.log(matching)
+    
+    updateSelection(first.sort());
+    updateSelection(matching.sort());
+    
+    }
 
 
+function updateSelection(matching) {
+
+    for(let x of matching) {
+        const li = document.createElement('li');
+        li.innerHTML = `<li>${x.country}</li>`;
+        list.appendChild(li);
+
+    }
 }
